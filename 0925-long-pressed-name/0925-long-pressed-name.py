82fadbe5e1ed1338810afaac48c07ad15390cb78
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-06 20:24:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (27.89%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i = 0
+        j = 0
+        n = len(name)
+        m = len(typed)
+        while i < n and j < m:
+            if name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif i > 0 and name[i - 1] == typed[j]:
+                j += 1
+            else:
+                return False
+        if i != n:
+            return False
+        while j < m:
+            if typed[j] != name[-1]:
+                return False
+            j += 1
+        return True
