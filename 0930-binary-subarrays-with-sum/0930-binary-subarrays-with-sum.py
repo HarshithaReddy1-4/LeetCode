f7dfe85bbdda1ec987b00f1cf9e567c0a34b92b6
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-09 11:07:18 +0530

    Time: 35 ms (54.26%), Space: 20.1 MB (63.26%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,33 +1,32 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        # def atMost(k):
-        #     if k < 0:
-        #         return 0
-        #     i = 0
-        #     s = 0
-        #     c = 0
+        def atMost(k):
+            if k < 0:
+                return 0
+            i = 0
+            s = 0
+            c = 0
 
-        #     for j in range(len(nums)):
-        #         s += nums[j]
-        #         while s > k:
-        #             s -= nums[i]
-        #             i += 1
-        #         c += (j - i + 1)
+            for j in range(len(nums)):
+                s += nums[j]
+                while s > k:
+                    s -= nums[i]
+                    i += 1
+                c += (j - i + 1)
             
-        #     return c
+            return c
 
-        # return atMost(goal) - atMost(goal - 1)
+        return atMost(goal) - atMost(goal - 1)
 
-        pref = {0: 1}
-        s = 0
-        ans = 0
-        for i in range(len(nums)):
-            s += nums[i]
+        # pref = {0: 1}
+        # s = 0
+        # ans = 0
+        # for i in range(len(nums)):
+        #     s += nums[i]
 
-            if s - goal in pref:
-                ans += pref[s - goal]
+        #     ans += pref.get(s - goal, 0)
             
-            pref[s] = pref.get(s, 0) + 1
+        #     pref[s] = pref.get(s, 0) + 1
         
-        return ans
+        # return ans
             
