f9e21ffe49be4be087454f4d30c061554213523c
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-13 09:56:35 +0530

    Time: 12 ms (22.99%), Space: 19.3 MB (53.39%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         rows, cols, posD, negD = set(), set(), set(), set()
-        board = [['.'] * n for _ in range(n)]
+        board = [[0] * n for _ in range(n)]
         res = 0
         def bt(i):
             nonlocal res
@@ -9,15 +9,15 @@ class Solution:
                 res += 1
                 return
             for j in range(n):
-                if i in rows or j in cols or i + j in posD or i - j in negD or board[i][j] == 'Q':
+                if i in rows or j in cols or i + j in posD or i - j in negD or board[i][j]:
                     continue
-                board[i][j] = 'Q'
+                board[i][j] = 1
                 rows.add(i)
                 cols.add(j)
                 posD.add(i + j)
                 negD.add(i - j)
                 bt(i + 1)
-                board[i][j] = '.'
+                board[i][j] = 0
                 rows.discard(i)
                 cols.discard(j)
                 posD.discard(i + j)
