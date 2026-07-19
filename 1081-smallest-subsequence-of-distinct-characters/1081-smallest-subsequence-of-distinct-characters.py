b36257351b87ee8ca47a78f51a58f78b3ce71a5d
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-19 11:34:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (75.23%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -3,16 +3,14 @@ class Solution:
         lastOcc = {c: i for i, c in enumerate(s)}
 
         stack = []
-        seen = set()
 
         for i, c in enumerate(s):
-            if c in seen:
+            if c in stack:
                 continue
             
             while stack and c < stack[-1] and i < lastOcc[stack[-1]]:
-                seen.remove(stack.pop())
+                stack.pop()
             
             stack.append(c)
-            seen.add(c)
         
         return ''.join(stack)
