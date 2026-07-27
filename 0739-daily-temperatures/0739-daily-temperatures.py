5327c40645e6ebbe1f1fa22872e382f81303ec01
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-27 19:55:28 +0530

    Time: 139 ms (9.33%), Space: 36.4 MB (5.72%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,5 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        d = defaultdict(int)
         stack = []
         n = len(temperatures)
         ans = [0] * n
