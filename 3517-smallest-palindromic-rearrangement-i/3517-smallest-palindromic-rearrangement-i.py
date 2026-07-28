21a15324d2b18e0181bbc6d57e8e39fe4c35ecee
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-07-28 18:25:38 +0530

    Time: 414 ms (18.89%), Space: 21 MB (51.15%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n <= 3:
+            return s
+        d = Counter(s)
+        half = ''.join(sorted(s[:n//2]))
+        if n % 2:
+            return half + s[n//2] + half[::-1]
+        return half + half[::-1]
