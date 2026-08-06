345302f2c7f12756a01ec886d75012cf09e6dcd1
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-08-06 18:53:37 +0530

    Time: 1 ms (28.86%), Space: 19.1 MB (99.19%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,15 +1,14 @@
 class Solution:
     def smallestNumber(self, n: int, t: int) -> int:
-        m = n
         for _ in range(10):
             prod = 1
-            temp = m
+            temp = n
             while temp > 0:
                 p = temp % 10
                 prod *= p
                 temp = temp // 10
             if prod % t == 0:
-                return m
-            m += 1
+                return n
+            n += 1
 
             
