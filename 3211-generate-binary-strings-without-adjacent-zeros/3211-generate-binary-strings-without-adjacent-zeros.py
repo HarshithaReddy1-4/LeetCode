6ed030a6919a329c694bdf9595e558cb7edd9dde
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-08-13 20:38:32 +0530

    Time: 60 ms (68.24%), Space: 20.8 MB (49.32%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.py
@@ -7,9 +7,9 @@ class Solution:
                 return
             if zero:
                 func(i + 1, s + '1', False)
-            else:
-                func(i + 1, s + '0', True)
-                func(i + 1, s + '1', False)
+                return
+            func(i + 1, s + '0', True)
+            func(i + 1, s + '1', False)
             
         func(0, '', False)
         return res
