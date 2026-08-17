cfba4e672aade40749ccecb3ac1ff14b19b3fba4
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-08-17 19:36:13 +0530

    Time: 3 ms (68.78%), Space: 19.4 MB (52.46%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        d = defaultdict(list)
+        res = []
+        n = len(groupSizes)
+
+        for i in range(n):
+            d[groupSizes[i]].append(i)
+            if len(d[groupSizes[i]]) == groupSizes[i]:
+                res.append(d[groupSizes[i]])
+                d[groupSizes[i]] = []
+        
+        res.sort(key = lambda x: len(x))
+        return res
