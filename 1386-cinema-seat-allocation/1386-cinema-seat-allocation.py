b6ace0da8fd52b932a7ad4b9d7fec72d5b39e09d
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-08-19 19:34:59 +0530

    Time: 67 ms (33.68%), Space: 23 MB (47.56%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
         seats = [[1, 2, 3, 4], [3, 4, 5, 6], [5, 6, 7, 8]]
-        rows = list(set([i for i, j in reservedSeats]))
+        rows = list(set([i - 1 for i, j in reservedSeats]))
         d = defaultdict(list)
         for i, j in reservedSeats:
             d[i - 1].append(j - 1)
@@ -9,7 +9,7 @@ class Solution:
         count = (n - len(rows)) * 2
         for i in rows:
             nums = [0] * 10
-            for i1 in d[i - 1]:
+            for i1 in d[i]:
                 nums[i1] = 1
             for j1, j2, j3, j4 in seats:
                 if nums[j1] == 0 and nums[j2] == 0 and nums[j3] == 0 and nums[j4] == 0:
