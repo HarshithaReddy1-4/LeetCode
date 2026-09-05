1897a5bd146dd1f578dcf9f2389de44c850dcf8f
HarshithaReddy1-4 <harshithareddy1476@gmail.com>
2026-09-05 20:18:44 +0530

    Time: 7 ms (48.33%), Space: 19.3 MB (31.34%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -2,7 +2,6 @@ class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
         maxx = []
-        minn = [-1] * n
         maxvar = float("-inf")
         for i in nums:
             maxvar = max(maxvar, i)
@@ -11,14 +10,12 @@ class Solution:
         minvar = float("inf")
         for i in range(n - 1, -1, -1):
             minvar = min(minvar, nums[i])
-            minn[i] = minvar
+            maxx[i] = maxx[i] - minvar
 
         idx = float("inf")
-        c = 0
-        for i, j in zip(maxx, minn):
-            if i - j <= k:
-                idx = min(idx, c)
-            c += 1
+        for i in range(n):
+            if maxx[i] <= k:
+                idx = min(idx, i)
 
         return idx if idx != float("inf") else -1
         
